d82ef8705fe1dea83d03466e69b322e982060e10
nihal <nihal.mulla121@gmail.com>
2022-09-16 16:27:01 +0530

    first commit

---
+++ src/com/test/PrimeNumber.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int i,n=100;
+		for (int i=1;I<=n;i++)
+		{
+			if(i÷7==0)
+				{
+					System.out.println(i);
+				}
+		}
+	}
+
+}
